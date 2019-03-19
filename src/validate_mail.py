6f7e3a6652a4e8c158b6fd05aa27776979054b55
Pavel Shvarchov <Shvarpa@gmail.com>
2019-03-19 11:51:37 +0200

    implemented and tested email validation

--- src/validate_mail.py
+++ src/validate_mail.py
@@ -1,16 +1,53 @@
 import requests
 import re
 
+class domain_api:
+    @staticmethod
+    def get(domain):
+        endpoint = "https://api.domainsdb.info/search"
+        query = {'query':domain}
+        return requests.get(endpoint,query).json()
+
 class Email:
     def __init__(self,email):
         self.email = email 
 
+    def __str__(self):
+        return str(self.email)
+
+    def __repr__(self):
+        return repr(self.email)
+
+    @staticmethod
+    def _match(email):
+        return re.match(r"^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)$", str(email))
+
     @property
     def domain(self):
-        return self.email.split('@')[1]
+        matches = Email._match(self)
+        if not matches: return None
+        return matches[1]
+
+    
+    def _valid_domain(self):
+        domain = self.domain
+        if not domain: return False
+        res = domain_api.get(domain)
+        if not res: return True
+        if res['total'] < 1: return False
+        found_dom = res['domains'][0]
+        if not re.match(found_dom['domain'],domain) : return False
+        if found_dom.get('isDead'):
+            if found_dom['isDead'] == True : return False
+        return True
+
 
     @property
     def is_valid(self):
-        matches = re.match(	r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",self.email)
-        return matches
+        in_format = Email._match(self)
+        if not in_format: return False
+        domain = self.domain
+        if not domain: return False
+        if not self._valid_domain(): return False
+        return True
 
