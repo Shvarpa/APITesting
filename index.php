d8399ce330f56a56c44f94a99c4c3a7448defa72
Pavel Shvarchov <Shvarpa@gmail.com>
2019-03-20 22:23:38 +0200

    added styles

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
 <?php include_once("index/README.html"); ?>
+<?php include_once("index/assets/css/github-markdown.css"); ?>
+<?php include_once("index/assets/css/hljs-github.min.css"); ?>
+<?php include_once("index/assets/css/pilcrow.css"); ?>
