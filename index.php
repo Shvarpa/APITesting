d4fa59752804bb6df9c420f15eb8fa3029b07d56
unknown <shvarpa@gmail.com>
2019-03-21 11:49:12 +0200

    removed useless lines in index.php

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
 <?php include_once("index/README.html"); ?>
-<?php include_once("index/assets/css/github-markdown.css"); ?>
-<?php include_once("index/assets/css/hljs-github.min.css"); ?>
-<?php include_once("index/assets/css/pilcrow.css"); ?>
